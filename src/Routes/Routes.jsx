1346b290dfed92f90f3f792bb2ef7918ff01bdc3
Juwelmia63 <juwelmia6364@gmail.com>
2025-10-08 06:37:29 -0700

    read list and wish list tab added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -22,6 +22,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '/listedbook',
+        loader : ()=> fetch('/booksData.json'),
         element: <Listedbooks></Listedbooks>
       },
       {
